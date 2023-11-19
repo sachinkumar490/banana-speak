7a29b53f3cf234414aef4d3cc9b6f3044465ef40
sachin <sachinkumarw59@gmail.com>
2023-11-20 00:29:40 +0530

    taking input and console

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // how to take input in js
-var username = prompt("give me username")
+// var username = prompt("give me username")
 
 // processing
-var welcomeMessage = "this is script works" +  username;
+// var welcomeMessage = "this is script works" +  username;
 
 // output
-alert(welcomeMessage);
+// alert(welcomeMessage);
+
+var btntranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input")
+console.log(txtInput)
+
+function clickHandler(){
+    console.log("clicked");
+    console.log("input",txtInput.value)
+};
+// syntax to click button function work  
+btntranslate.addEventListener("click",clickHandler)
+    
