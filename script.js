f0ba71a5bc72424387d73d84c8fab803eb2ad4a6
sachin <sachinkumarw59@gmail.com>
2023-11-21 00:49:31 +0530

    translation complete

--- script.js
+++ script.js
@@ -11,10 +11,28 @@ var btntranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output");
 
-
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+function getTranslationURL (text) {
+    return serverURL + "?" + "text=" + text
+}
+// when an error occurs
+function errorHandler(error){
+    console.log("error occured", error)
+}
+// when click happens
 function clickHandler(){
-  outputDiv.innerText = "gsdzcbxvgsdf" + " " + txtInput.value; 
-    
+    var inputText = txtInput.value;
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(Response => Response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+
 };
 // syntax to click button function work  
 btntranslate.addEventListener("click",clickHandler)
